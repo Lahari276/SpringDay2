5abdf56e44939e1c54eb6c12fee25ea4241c039e
Lahari276 <laharireddy147@gmail.com>
2021-04-20 12:54:04 +0530

    @ComponentScan onn annoconfig package

--- SpringProject 2/src/annoconfig/AppConfig.java
+++ SpringProject 2/src/annoconfig/AppConfig.java
@@ -1,16 +1,18 @@
 package annoconfig;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "annoconfig")
 public class AppConfig {
 	
-	@Bean
+	/*@Bean
 	public Samsung getPhone() {
 		return new Samsung();
 	}
 	@Bean
 	public MobileProcessor getCpu() {
 		return new Snapdragon();
-	}
+	}*/
 }
