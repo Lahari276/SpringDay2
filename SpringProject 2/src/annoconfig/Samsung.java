182cebebe86f57c3c94e3c0c4221349b40039eeb
Lahari276 <laharireddy147@gmail.com>
2021-04-20 12:44:55 +0530

    Annotation using beans

--- SpringProject 2/src/annoconfig/Samsung.java
+++ SpringProject 2/src/annoconfig/Samsung.java
@@ -6,19 +6,13 @@ public class Samsung {
 	@Autowired
 	MobileProcessor cpu;
 	
-	
-	
 	public MobileProcessor getCpu() {
 		return cpu;
 	}
 
-
-
 	public void setCpu(MobileProcessor cpu) {
 		this.cpu = cpu;
 	}
-
-
 
 	public void config() {
 		System.out.println("octa core-- 8 gb");
