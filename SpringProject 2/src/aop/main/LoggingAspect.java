69da222b54405a9748a34fce06cbfcd20dec8f62
Lahari276 <laharireddy147@gmail.com>
2021-04-21 11:10:21 +0530

    Getters method in wildcards

--- SpringProject 2/src/aop/main/LoggingAspect.java
+++ SpringProject 2/src/aop/main/LoggingAspect.java
@@ -2,19 +2,34 @@ package aop.main;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	//one aspect can contain multiple advices
 	
-	
-	//@Before("execution(* aop.model..*.*(..))")
-	//@Before("execution(* aop..*.*(..))")
 	//@Before("execution(public String aop.model.Triangle.getName())")
-	@Before("execution(public * aop.model.*.get*())")
+	
+		//@Before("execution(* com.aop..*.*(..))")
+		//@Before("execution(public * aop.model.*.getName())")
+		//@Before("execution(public * aop.model.*.getName())")
 
-	public void loggingAdvice() {
-		System.out.println("writing log before method is executed");
-	}
-
+		//@Before("execution(* aop..*.*(..))")//for aop and its subpackages
+		
+		/*@Before("execution(* aop.model.*.get())")
+		public void loggingAdvice() {
+			System.out.println("writing log before method is executed");
+		}
+		@Before("execution(* aop.model.*.get())")
+		public void secondAdviceforAllGetters() {
+			System.out.println("second log before method is executed");
+		}
+		
+		@Before("execution(* aop.model.*.get())")
+		public void thirdAdviceforAllGetters() {
+			System.out.println("third log before method is executed");
+		} */
+		
+		@Pointcut("execution(* aop..*.get*())")
+		public void allGetters() {}
 }
